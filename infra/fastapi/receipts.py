e5ca600ad23ba006f0804c0b429f1792da8d49a8
GuRgEnA <agurg21@freeuni.edu.ge>
2023-12-29 21:28:00 +0400

    Refactoring

--- infra/fastapi/receipts.py
+++ infra/fastapi/receipts.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from core.errors import AlreadyExistError, DoesNotExistError, ClosedReceiptError
+from core.errors import DoesNotExistError, ClosedReceiptError, ErrorMessageEnvelope
 from core.receipt import Receipt
 from infra.fastapi.dependables import ReceiptRepositoryDependable
 
@@ -44,18 +44,16 @@ class EmptyResponse(BaseModel):
 @receipt_api.post("/receipts",
                   status_code=201,
                   response_model=ReceiptItemEnvelope)
-def create_product(receipts: ReceiptRepositoryDependable):
+def create_receipt(receipts: ReceiptRepositoryDependable):
     receipt = Receipt()
-    try:
-        receipts.create(receipt)
-        return {"receipt": receipt}
-    except AlreadyExistError as e:
-        return e.get_error_json_response(409)
+    receipts.create(receipt)
+    return {"receipt": receipt}
 
 
 @receipt_api.post("/receipts/{receipt_id}/products",
                   status_code=201,
-                  response_model=ReceiptItemEnvelope)
+                  response_model=ReceiptItemEnvelope,
+                  responses={404: {'model': ErrorMessageEnvelope}})
 def add_product(receipt_id: UUID, req: AddProductItem, receipts: ReceiptRepositoryDependable):
     try:
         return {"receipt": receipts.add_product(receipt_id, req.id, req.quantity)}
@@ -65,8 +63,9 @@ def add_product(receipt_id: UUID, req: AddProductItem, receipts: ReceiptReposito
 
 @receipt_api.get("/receipts/{receipt_id}",
                  status_code=200,
-                 response_model=ReceiptItemEnvelope)
-def read_by_id(receipt_id: UUID, receipts: ReceiptRepositoryDependable):
+                 response_model=ReceiptItemEnvelope,
+                 responses={404: {'model': ErrorMessageEnvelope}})
+def read_receipt(receipt_id: UUID, receipts: ReceiptRepositoryDependable):
     try:
         return {"receipt": receipts.read(receipt_id)}
     except DoesNotExistError as e:
@@ -75,8 +74,9 @@ def read_by_id(receipt_id: UUID, receipts: ReceiptRepositoryDependable):
 
 @receipt_api.patch("/receipts/{receipt_id}",
                    status_code=200,
-                   response_model=EmptyResponse)
-def read_by_id(receipt_id: UUID, req: UpdateReceiptStatusItem, receipts: ReceiptRepositoryDependable):
+                   response_model=EmptyResponse,
+                   responses={404: {'model': ErrorMessageEnvelope}})
+def update_receipt(receipt_id: UUID, req: UpdateReceiptStatusItem, receipts: ReceiptRepositoryDependable):
     try:
         receipts.update_status(receipt_id, req.status)
         return {}
@@ -86,8 +86,9 @@ def read_by_id(receipt_id: UUID, req: UpdateReceiptStatusItem, receipts: Receipt
 
 @receipt_api.delete("/receipts/{receipt_id}",
                     status_code=200,
-                    response_model=EmptyResponse)
-def read_by_id(receipt_id: UUID, receipts: ReceiptRepositoryDependable):
+                    response_model=EmptyResponse,
+                    responses={403: {'model': ErrorMessageEnvelope}, 404: {'model': ErrorMessageEnvelope}})
+def delete_receipt(receipt_id: UUID, receipts: ReceiptRepositoryDependable):
     try:
         receipts.delete(receipt_id)
         return {}
